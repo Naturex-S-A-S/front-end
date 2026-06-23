40d2131145c7d8f934a6142e0ee7d70ded2a28e0
Deiby Moreno <d.moreno@visiontravel.net>
2026-06-23 10:33:09 -0500

    [FEAT] Enhance packaging material form: add category validation and error handling

--- src/utils/schemas/inventory/packagingMaterial.ts
+++ src/utils/schemas/inventory/packagingMaterial.ts
@@ -8,6 +8,14 @@ export const packagingMaterialSchema = yup
       .min(0, "El minimo estandar debe ser mayor o igual a 0")
       .typeError("El minimo estandar debe ser un número")
       .required("El minimo estandar es requerido"),
-    color: yup.string().optional()
+    color: yup.string().optional(),
+    category: yup
+      .array()
+      .of(
+        yup.object({
+          id: yup.string().required("El categoría es requerida")
+        })
+      )
+      .required("Las categorías son requeridas")
   })
   .required();
