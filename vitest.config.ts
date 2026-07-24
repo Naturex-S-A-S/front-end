f668bd0508a9e6aa80e53eea42278015b88a0809
Deiby Moreno <d.moreno@visiontravel.net>
2026-07-24 16:10:52 -0500

    CHANGE Vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,36 +1,33 @@
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/default */
-/* eslint-disable import/namespace */
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   resolve: {
-    tsconfigPaths: true,
+    tsconfigPaths: true
   },
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/utils/tests/setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/utils/tests/setup.ts"],
     globals: true,
+    reporters: ["default"],
     css: true,
     clearMocks: true,
-    exclude: ['node_modules', 'tests'],
+    exclude: ["node_modules", "tests"],
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-      include: ['src/**/*.{ts,tsx}'],
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      include: ["src/**/*.{ts,tsx}"],
       exclude: [
-        'src/**/*.d.ts',
-        'src/**/types.ts',
-        'src/**/*.test.{ts,tsx}',
-        'src/**/__tests__/**',
-        'src/utils/tests/**',
-        'src/assets/**',
-        'src/configs/**',
-      ],
-    },
-  },
-})
+        "src/**/*.d.ts",
+        "src/**/types.ts",
+        "src/**/*.test.{ts,tsx}",
+        "src/**/__tests__/**",
+        "src/utils/tests/**",
+        "src/assets/**",
+        "src/configs/**"
+      ]
+    }
+  }
+});
